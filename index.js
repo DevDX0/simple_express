5de948ad623b1af0286ffd9d1a57ea91717977fd
DevDX <hatuan1997hd@gmail.com>
2023-03-23 10:47:19 +0700

    Init commit

---
+++ index.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var app = express();
+
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
+app.get("/test", function (req, res) {
+  console.log("Got a GET request for /test");
+  res.send({ code: 200, message: "Got a GET request for /test" });
+});
+
+var server = app.listen(8080, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log("Server running at http://%s:%s", host, port);
+});
